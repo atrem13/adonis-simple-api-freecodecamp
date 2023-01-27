3e5232c28d05ed5bc54d86c26a28e58e5598ed48
atrem13 <mertayoga17.my@gmail.com>
2023-01-27 16:51:03 +0800

    update

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,3 +1,14 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+export default class AuthController {
+    public async login({request, auth}: HttpContextContract){
+        const email = request.input('email')
+        const password = request.input('password')
+        const token = await auth.use('api').attempt(email, password, {
+            expiresIn: "1 days",   
+        })
+        return token.toJSON()
+    }
 
-export default class AuthController {}
+    
+}
